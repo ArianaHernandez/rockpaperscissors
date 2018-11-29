5edcc2eb717757af6443b7e7c3e89426c198bcaf
Ariana Hernandez <arianahernandez@whsat.org>
2018-11-29 23:19:17 +0000

    AddedMoreStuffIGuess

--- js/script.js
+++ js/script.js
@@ -17,50 +17,63 @@ return compChoice[num];
 }
 
 $("#shoot").click(function(){
+    var userpoint=0;
+    var compchoice=0;
     var userchoice = $("input").val();
     $("#userChoice").text(userchoice);
     var compPlay = computerChoice();
     $("#computerChoice").text(compPlay);
+    
+    function startImageReturn(){
+      document.getElementById('starterImage').src='Start.gif';
+    }
 
-   if(compPlay==="paper"&&userchoice==="paper"){
+   if(compPlay==="paper"&&userchoice==="paper"||compPlay==="rock"&&userchoice==="rock"||compPlay==="scissors"&&userchoice==="scissors"){
         winner="You Tie";
-    $("#result").text(winner); 
-    document.getElementById('starterImage').src='paperpaper.png';
-   }
-   else if(compPlay==="rock"&&userchoice==="rock"){
-          winner="You Tie";
-        $("#result").text(winner);  
-        document.getElementById('starterImage').src='rockrock.png';
-   }else if(compPlay==="scissors"&&userchoice==="scissors" ){
-        winner="You Tie";
+       $("#result").text(winner); 
+           if(compPlay==="rock"){
+             document.getElementById('starterImage').src='rockrock.png';
+            setTimeout(startImageReturn, 1000);
+   }else if(compPlay==="scissors"){
          document.getElementById('starterImage').src='scisscis.png';
-        $("#result").text(winner); 
-    }else if(compPlay==="paper"&&userchoice==="scissors"){
+          setTimeout(startImageReturn, 1000);
+   }else{
+     document.getElementById('starterImage').src='paperpaper.png';
+      setTimeout(startImageReturn, 1000);
+   }
+   }else if(compPlay==="paper"&&userchoice==="scissors"){
           winner="You Win";
         $("#result").text(winner); 
          document.getElementById('starterImage').src='scispaper.png';
+          setTimeout(startImageReturn, 1000);
    }else if(compPlay==="scissors"&&userchoice==="rock"){
           winner="You Win";
         $("#result").text(winner);  
          document.getElementById('starterImage').src='rockscis.png';
+          setTimeout(startImageReturn, 1000);
    } else if(compPlay==="rock"&&userchoice==="paper"){
          winner="You Win";
         $("#result").text(winner);  
          document.getElementById('starterImage').src='paperrock.png';
+          setTimeout(startImageReturn, 1000);
    }else if(compPlay==="scissors"&&userchoice==="paper"){
     winner="Computer Wins";
     document.getElementById('starterImage').src='paperscis.png';
     $("#result").text(winner);
+     setTimeout(startImageReturn, 1000);
    }
+
    else if(compPlay==="rock"&&userchoice==="scissors"){
           winner="Computer Wins";
     $("#result").text(winner);
      document.getElementById('starterImage').src='scisrock.png';
+      setTimeout(startImageReturn, 1000);
    }
    else if(compPlay==="paper"&&userchoice==="rock"){
           winner="Computer Wins";
     $("#result").text(winner);
      document.getElementById('starterImage').src='rockpaper.png';
+      setTimeout(startImageReturn, 1000);
    }
    else{
       winner="No One Wins";
