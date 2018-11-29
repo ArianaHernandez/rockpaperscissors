303e6839b8f5ef88a8c7ba3a89bfaef9f19ff386
Ariana Hernandez <arianahernandez@whsat.org>
2018-11-29 20:33:00 +0000

    AlmostDone

--- js/script.js
+++ js/script.js
@@ -23,14 +23,44 @@ $("#shoot").click(function(){
     $("#computerChoice").text(compPlay);
 
    if(compPlay==="paper"&&userchoice==="paper"){
+        winner="You Tie";
+    $("#result").text(winner); 
+    document.getElementById('starterImage').src='paperpaper.png';
+   }
+   else if(compPlay==="rock"&&userchoice==="rock"){
           winner="You Tie";
-    $("#result").text(winner);  
-   }else if(compPlay==="rock"&&userchoice==="rock"){
-          winner="You Tie";
-    $("#result").text(winner);  
-   }else if(compPlay==="scissors"&&userchoice==="scissors"){
-          winner="You Tie";
-    $("#result").text(winner);  
+        $("#result").text(winner);  
+        document.getElementById('starterImage').src='rockrock.png';
+   }else if(compPlay==="scissors"&&userchoice==="scissors" ){
+        winner="You Tie";
+         document.getElementById('starterImage').src='scisscis.png';
+        $("#result").text(winner); 
+    }else if(compPlay==="paper"&&userchoice==="scissors"){
+          winner="You Win";
+        $("#result").text(winner); 
+         document.getElementById('starterImage').src='scispaper.png';
+   }else if(compPlay==="scissors"&&userchoice==="rock"){
+          winner="You Win";
+        $("#result").text(winner);  
+         document.getElementById('starterImage').src='rockscis.png';
+   } else if(compPlay==="rock"&&userchoice==="paper"){
+         winner="You Win";
+        $("#result").text(winner);  
+         document.getElementById('starterImage').src='paperrock.png';
+   }else if(compPlay==="scissors"&&userchoice==="paper"){
+    winner="Computer Wins";
+    document.getElementById('starterImage').src='paperscis.png';
+    $("#result").text(winner);
+   }
+   else if(compPlay==="rock"&&userchoice==="scissors"){
+          winner="Computer Wins";
+    $("#result").text(winner);
+     document.getElementById('starterImage').src='scisrock.png';
+   }
+   else if(compPlay==="paper"&&userchoice==="rock"){
+          winner="Computer Wins";
+    $("#result").text(winner);
+     document.getElementById('starterImage').src='rockpaper.png';
    }
    else{
       winner="No One Wins";
